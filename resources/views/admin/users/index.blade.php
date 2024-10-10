9bd96483142fbea32e16b01cd24c244c252aa30d
Assis Vasconcelos <assisvascconcelos@gmail.com>
2024-10-10 12:25:07 -0300

    listar-registros

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,11 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Usuários</title>
 </head>
 <body>
-    <h1>Olá {{ $user->name }}</h1>
+    <h1>Lista de Usuários</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $user)
+                <tr>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>-</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </body>
 </html>
